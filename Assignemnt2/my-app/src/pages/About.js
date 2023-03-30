a22dd3357eeda07ce2b67c48f487e5232a52a457
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-03-30 21:53:05 +0200

    b

---
+++ Assignemnt2/my-app/src/pages/About.js
@@ -0,0 +1,5 @@
+function About() {
+  return <h1>Welcome to the Home Page!</h1>;
+}
+
+export default About;
---
+++ Assignemnt2/my-app/src/pages/Contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+  return <h1>Welcome to the Home Page!</h1>;
+}
+
+export default Contact;
