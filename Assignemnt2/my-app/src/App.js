b35516bd1a231f15fcb0135629494ee4a3626ce9
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-03-30 22:29:28 +0200

    Route

--- Assignemnt2/my-app/src/App.js
+++ Assignemnt2/my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -6,11 +6,11 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <BrowserRouter basename="/my-app">
-    <Switch>
+    <Routes>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
-    </Switch>
+    </Routes>
   </BrowserRouter>
   );
 }
