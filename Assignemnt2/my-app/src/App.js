93530876d3abd39d08bb242ad4277d50da0b4dbe
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-03-30 22:28:27 +0200

    s

--- Assignemnt2/my-app/src/App.js
+++ Assignemnt2/my-app/src/App.js
@@ -1,23 +1,17 @@
-import './App.css';
-import AppRoutes from './routes';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <AppRoutes />
-      </main>
-    </div>
+    <BrowserRouter basename="/my-app">
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
+  </BrowserRouter>
   );
 }
 
