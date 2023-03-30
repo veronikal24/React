acb762b75047a6e794f73eb1669f480beb4891c5
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-03-30 22:23:03 +0200

    basename

--- Assignemnt2/my-app/src/routes.js
+++ Assignemnt2/my-app/src/routes.js
@@ -1,18 +1,22 @@
 
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 
+  
 function AppRoutes() {
+  const basename = "/my-app"; // define basename here
   return (
+    <BrowserRouter basename={basename}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    </BrowserRouter>
   );
 }
 
