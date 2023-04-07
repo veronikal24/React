f0c58411876f13d5ee5a3640eb92e6ed937a80ee
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-07 21:21:16 +0200

    path

--- Assignemnt2/my-app/src/index.js
+++ Assignemnt2/my-app/src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import NotFound from './NotFound';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/React"> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
