0b8561ab368c39557eacb6bf5559013125d9ec56
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-07 21:02:47 +0200

    vv

--- Assignemnt2/my-app/src/Home.js
+++ Assignemnt2/my-app/src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home">
   
 <h1>This is a HOME PAGE</h1>
-<h2>This is heading 2</h2>
+<h2>This is HOOOME</h2>
 <h3>This is heading 3</h3>
 <h4>This is heading 4</h4>
 <h5>This is heading 5</h5>
