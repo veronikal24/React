5041fbd84f90dbc27f84a8de425183c87321e814
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 18:04:53 +0200

    styling

--- Assignemnt2/my-app/src/Pokedex.js
+++ Assignemnt2/my-app/src/Pokedex.js
@@ -5,42 +5,41 @@ function Pokedex() {
   const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    async function fetchPokemonData() {
-      const pokemonData = await Promise.all([
-        fetchPokemonDataByName("bulbasaur"),
-        fetchPokemonDataByName("ivysaur"),
-        fetchPokemonDataByName("venusaur"),
-        fetchPokemonDataByName("charmander"),
-        fetchPokemonDataByName("charmeleon"),
-        fetchPokemonDataByName("charizard"),
-        fetchPokemonDataByName("squirtle"),
-        fetchPokemonDataByName("wartortle"),
-        fetchPokemonDataByName("blastoise"),
-        fetchPokemonDataByName("caterpie"),
-        fetchPokemonDataByName("metapod"),
-        fetchPokemonDataByName("butterfree"),
-        fetchPokemonDataByName("pidgeotto"),
+    async function fetchPokemon() {
+      const responses = await Promise.all([
+        getPokemon("bulbasaur"),
+        getPokemon("ivysaur"),
+        getPokemon("venusaur"),
+        getPokemon("charmander"),
+        getPokemon("charmeleon"),
+        getPokemon("charizard"),
+        getPokemon("squirtle"),
+        getPokemon("wartortle"),
+        getPokemon("blastoise"),
+        getPokemon("caterpie"),
+        getPokemon("metapod"),
+        getPokemon("butterfree"),
+        getPokemon("pidgeotto"),
       ]);
 
+      const data = await Promise.all(responses.map((res) => res.json()));
+      const pokemonData = data.map((pokemon) => ({
+        name: pokemon.name,
+        weight: pokemon.weight / 10,
+        image: pokemon.sprites.front_default,
+      }));
       setPokemonList(pokemonData);
     }
 
-    fetchPokemonData();
+    fetchPokemon();
   }, []);
 
-  async function fetchPokemonDataByName(name) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const data = await response.json();
-
-    return {
-      name: data.name,
-      weight: data.weight,
-      image: data.sprites.front_default,
-    };
+  function getPokemon(name) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   }
 
   return (
-    <div>
+    <div className="pokedex">
       <h2>POKEDEX PAGE, See All the Cool Pokemons you can use</h2>
       <nav>
         <ul>
@@ -50,17 +49,19 @@ function Pokedex() {
             </Link>
           </li>
         </ul>
-        {pokemonList.map((pokemon) => (
-  <div key={pokemon.name} style={{ border: '1px solid black', padding: '10px', marginBottom: '10px' }}>
-    <h3 style={{ color: 'blue', fontSize: '20px' }}>{pokemon.name}</h3>
-    <p>Weight: {pokemon.weight}</p>
-    <img style={{ maxWidth: '200px' }} src={pokemon.image} alt={`${pokemon.name} sprite`} />
-  </div>
-))}
-
       </nav>
+      <div className="pokedex-list">
+        {pokemonList.map((pokemon) => (
+          <div key={pokemon.name} className="pokemon">
+            <h3>{pokemon.name}</h3>
+            <p>Weight: {pokemon.weight}</p>
+            <img src={pokemon.image} alt={`${pokemon.name} sprite`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Pokedex;
+
