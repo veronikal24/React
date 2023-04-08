112a35e33a895601c4ebaac3d8ef575a777edcc0
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 20:39:24 +0200

    ver

--- Assignemnt2/my-app/src/About.js
+++ Assignemnt2/my-app/src/About.js
@@ -3,13 +3,13 @@ import { useLocation } from 'react-router-dom';
 function About() {
   const { search } = useLocation();
   const params = new URLSearchParams(search);
-  const id = params.get('name');
+  const name = params.get('name');
 
   // Use the `id` to fetch data about the Pokemon from an API or elsewhere
 
   return (
     <div>
-      <h2>About Pokemon {id}</h2>
+      <h2>About Pokemon {name}</h2>
       {/* Display data about the Pokemon */}
     </div>
   );
