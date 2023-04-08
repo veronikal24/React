8276d02c47de8e58324a4989752849bac12b0f95
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 19:25:20 +0200

    v

--- Assignemnt2/my-app/src/App.js
+++ Assignemnt2/my-app/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Pokedex from './Pokedex';
 import About from './About';
 
 function App() {
   return (
-    <div>
-    <Route path="/" element={<Pokedex />} />
-    <Route path="/about/:id" element={<About />} />
-  </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Pokedex />} />
+      <Route path="/about/:id" element={<About />} />
+    </Routes>
+  </Router>
   );
 }
 
