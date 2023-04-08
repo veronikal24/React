c5b6e9200edc96bddc821f4e0513386ce07ce267
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 18:57:01 +0200

    poke

--- Assignemnt2/my-app/src/Pokedex.js
+++ Assignemnt2/my-app/src/Pokedex.js
@@ -1,59 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './index.css'; // import the CSS file
+import './Pokedex.css';
 
 function Pokedex() {
   const [pokemonList, setPokemonList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pokemonPerPage] = useState(6);
+  const [page, setPage] = useState(1);
   const history = useNavigate();
 
   useEffect(() => {
     async function fetchPokemonList() {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=18&offset=${(page - 1) * 18}`);
       const data = await response.json();
-      const promises = data.results.map((result) => fetch(result.url).then((res) => res.json()));
-      const pokemonDataList = await Promise.all(promises);
-      setPokemonList(pokemonDataList);
+      const promises = data.results.map((result) => fetch(result.url).then(response => response.json()));
+      const pokemonData = await Promise.all(promises);
+      setPokemonList(pokemonData);
     }
 
     fetchPokemonList();
-  }, []);
+  }, [page]);
 
-  const handleCardClick = (pokemon) => {
-    history.push(`/about/${pokemon.name}`, { pokemonData: pokemon });
-  };
-
-  const indexOfLastPokemon = currentPage * pokemonPerPage;
-  const indexOfFirstPokemon = indexOfLastPokemon - pokemonPerPage;
-  const currentPokemon = pokemonList.slice(indexOfFirstPokemon, indexOfLastPokemon);
-
-  const renderPokemon = () => {
-    return currentPokemon.map((pokemon) => (
-      <div key={pokemon.id} className="pokemon-card" onClick={() => handleCardClick(pokemon)}>
-        <h3>{pokemon.name}</h3>
-        <img src={pokemon.sprites.front_default} alt={`${pokemon.name} sprite`} />
-      </div>
-    ));
-  };
-
-  const renderPagination = () => {
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(pokemonList.length / pokemonPerPage); i++) {
-      pageNumbers.push(i);
+  function handlePrevPage() {
+    if (page > 1) {
+      setPage(page - 1);
     }
-    return pageNumbers.map((number) => (
-      <li key={number}>
-        <button onClick={() => setCurrentPage(number)}>{number}</button>
-      </li>
-    ));
-  };
+  }
+
+  function handleNextPage() {
+    setPage(page + 1);
+  }
 
   return (
     <div>
       <h2>POKEDEX PAGE, See All the Cool Pokemons you can use</h2>
-      <div className="pokemon-container">{renderPokemon()}</div>
-      <ul className="pagination">{renderPagination()}</ul>
+      <nav>
+        <ul className="pokemon-list">
+          {pokemonList.map((pokemon) => (
+            <li key={pokemon.id} className="pokemon-card">
+              <Link to={`/about/${pokemon.id}`}>
+                <h3>{pokemon.name}</h3>
+                <img src={pokemon.sprites.front_default} alt={`${pokemon.name} sprite`} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="pagination">
+          <button onClick={handlePrevPage}>Prev</button>
+          <span>{page}</span>
+          <button onClick={handleNextPage}>Next</button>
+        </div>
+      </nav>
     </div>
   );
 }
