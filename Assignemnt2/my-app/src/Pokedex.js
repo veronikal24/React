c55ba30028d3dd2f5fae81d705b81f1b88195b76
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 18:11:07 +0200

    styling

--- Assignemnt2/my-app/src/Pokedex.js
+++ Assignemnt2/my-app/src/Pokedex.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import './myStyles.css';
 function Pokedex() {
   const [pokemonList, setPokemonList] = useState([]);
 
