e4bfb5bf02340769a227075f69c5529b952d4202
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 19:34:10 +0200

    router change

--- Assignemnt2/my-app/src/index.js
+++ Assignemnt2/my-app/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Pokedex from './Pokedex';
 import About from './About';
-import NotFound from './NotFound';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="/React"> 
-      <Routes>
-        <Route path="/" element={<Pokedex />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+
+    <Routes>
+      <Route path="/" element={<Pokedex />} />
+      <Route path="/about/:id" element={<About />} />
+    </Routes>
+
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
