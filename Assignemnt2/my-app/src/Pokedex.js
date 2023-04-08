f8e8a43f11a78a5a314ec710e5df4dfaf7ba9016
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 18:46:42 +0200

    another change

--- Assignemnt2/my-app/src/Pokedex.js
+++ Assignemnt2/my-app/src/Pokedex.js
@@ -8,63 +8,59 @@ import { Link, useNavigate} from 'react-router-dom';
 function Pokedex() {
   const [pokemonList, setPokemonList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 12;
+  const itemsPerPage = 8;
   const history = useNavigate();
   useEffect(() => {
     async function fetchPokemonList() {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
       const data = await response.json();
-      setPokemonList(data.results);
+      const promises = data.results.map((result) => fetch(result.url).then((res) => res.json()));
+      const pokemonDataList = await Promise.all(promises);
+      setPokemonList(pokemonDataList);
     }
 
     fetchPokemonList();
   }, []);
 
-  function handleNextPage() {
-    setCurrentPage(currentPage + 1);
-  }
-  function handlePrevPageClick() {
-    setCurrentPage(currentPage - 1);
-  }
+
+  const handleCardClick = (pokemon) => {
+    history.push(`/about/${pokemon.name}`, { pokemonData: pokemon });
+  };
 
    // Determining how many pokemons per page we need to see
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currentPokemonList = pokemonList.slice(startIndex, endIndex);
+   // Determining 
+  const indexOfLastPokemon = currentPage * itemsPerPage;
+  const indexOfFirstPokemon = indexOfLastPokemon - itemsPerPage;
+  const currentPokemon = pokemonList.slice(indexOfFirstPokemon, indexOfLastPokemon);
 
+  const renderPokemon = () => {
+    return currentPokemon.map((pokemon) => (
+      <div key={pokemon.id} className="pokemon-card" onClick={() => handleCardClick(pokemon)}>
+        <h3>{pokemon.name}</h3>
+        <img src={pokemon.sprites.front_default} alt={`${pokemon.name} sprite`} />
+      </div>
+    ));
+  };
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(pokemonList.length / itemsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers.map((number) => (
+      <li key={number}>
+        <button onClick={() => setCurrentPage(number)}>{number}</button>
+      </li>
+    ));
+  };
   return (
     <div>
       <h2>POKEDEX PAGE, See All the Cool Pokemons you can use</h2>
-      
-      <nav>
-        <ul>
-          <li>
-            <Link to="/about">
-              Click on a specific picture to go to About page
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="pokemon-list">
-        {currentPokemonList.map((pokemon) => (
-          <div key={pokemon.name} className="pokemon-item">
-            <h3>{pokemon.name}</h3>
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${startIndex + pokemonList.indexOf(pokemon) + 1}.png`}
-              alt={`${pokemon.name} sprite`}
-              className="pokemon-image"
-            />
-          </div>
-        ))}
-      </div>
-   
-      <div className="pagination">
-        <button onClick={handlePrevPageClick} disabled={currentPage === 1}>Prev</button>
-        <button onClick={handleNextPage} disabled={endIndex >= pokemonList.length}>Next</button>
-      </div>
- 
+      <div className="pokemon-container">{renderPokemon()}</div>
+      <ul className="pagination">{renderPagination()}</ul>
     </div>
-
   );
 }
 
