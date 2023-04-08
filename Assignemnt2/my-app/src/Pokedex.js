7c0d78d570313176ba990e4a77df54411b4d8e06
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 18:30:26 +0200

    also back

--- Assignemnt2/my-app/src/Pokedex.js
+++ Assignemnt2/my-app/src/Pokedex.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './index.css'; // import the CSS file
-import { Link } from 'react-router-dom';
+import { Link, useHistory} from 'react-router-dom';
 
 
 
 function Pokedex() {
   const [pokemonList, setPokemonList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-
+  const itemsPerPage = 12;
+  const history = useHistory();
   useEffect(() => {
     async function fetchPokemonList() {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon');
@@ -21,6 +22,10 @@ function Pokedex() {
   function handleNextPage() {
     setCurrentPage(currentPage + 1);
   }
+  function handlePrevPageClick() {
+    setCurrentPage(currentPage - 1);
+  }
+
 
   const startIndex = (currentPage - 1) * 6;
   const endIndex = startIndex + 6;
@@ -51,12 +56,23 @@ function Pokedex() {
           </div>
         ))}
       </div>
-      <div className="next-button">
-        {currentPage < Math.ceil(pokemonList.length / 6) && (
-          <button onClick={handleNextPage}>Next</button>
-        )}
+   
+      <div className="pagination">
+        <button onClick={handlePrevPageClick} disabled={currentPage === 1}>Prev</button>
+        <button onClick={handleNextPage} disabled={endIndex >= pokemonList.length}>Next</button>
       </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">
+              Click on a specific picture to go to About page
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <button onClick={() => history.goBack()}>Go Back</button>
     </div>
+
   );
 }
 
