ec0ec1a2ade07bdb0450f5b484f19af82977e504
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 22:19:23 +0200

    id check

--- Assignemnt2/my-app/src/About.js
+++ Assignemnt2/my-app/src/About.js
@@ -10,7 +10,10 @@ function About() {
 
   useEffect(() => {
     async function fetchPokemonData() {
+      console.log("this is the id")
+      console.log(id)
       if (!id) return; // add this check to ensure id is not null
+      console.log(id)
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}?fields=name,height,weight,abilities,sprites`);
       const data = await response.json();
       setPokemonData(data);
