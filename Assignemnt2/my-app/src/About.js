56637074bc877d9cb3b4c481e3969d9944801e8b
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 19:36:15 +0200

    about pokemon

--- Assignemnt2/my-app/src/About.js
+++ Assignemnt2/my-app/src/About.js
@@ -1,5 +1,4 @@
-
-
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function About() {
@@ -7,15 +6,43 @@ function About() {
   const params = new URLSearchParams(search);
   const id = params.get('id');
 
-  // Use the `id` to fetch data about the Pokemon from an API or elsewhere
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    async function fetchPokemonData() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      setPokemonData(data);
+    }
+    fetchPokemonData();
+  }, [id]);
+
+  if (!pokemonData) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, sprites, height, weight, abilities, moves } = pokemonData;
 
   return (
     <div>
-      <h2>About Pokemon {id}</h2>
-      {/* Display data about the Pokemon */}
+      <h2>About Pokemon {name}</h2>
+      <img src={sprites.front_default} alt={`${name} sprite`} />
+      <p>Height: {height}</p>
+      <p>Weight: {weight}</p>
+      <p>Abilities:</p>
+      <ul>
+        {abilities.map((ability) => (
+          <li key={ability.ability.name}>{ability.ability.name}</li>
+        ))}
+      </ul>
+      <p>Moves:</p>
+      <ul>
+        {moves.map((move) => (
+          <li key={move.move.name}>{move.move.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default About;
-
