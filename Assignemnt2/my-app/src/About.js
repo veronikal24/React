202b64cbdf24c0a504a1fee5391c2cfcda3dc673
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-04-08 20:27:37 +0200

    errors from api

--- Assignemnt2/my-app/src/About.js
+++ Assignemnt2/my-app/src/About.js
@@ -7,31 +7,50 @@ function About() {
   const id = params.get('id');
 
   const [pokemonData, setPokemonData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchPokemonData() {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}?fields=name,height,weight,abilities`);
-      const data = await response.json();
-      setPokemonData(data);
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}?fields=name,height,weight,abilities,sprites`);
+        const data = await response.json();
+        setPokemonData(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     }
     fetchPokemonData();
   }, [id]);
 
-  if (!pokemonData) {
+  if (loading) {
     return <div><h2>Loading...</h2></div>;
   }
 
-  const { name, sprites, height, weight } = pokemonData;
+  if (error) {
+    return <div><h2>Error: {error}</h2></div>;
+  }
+
+  const { name, sprites, height, weight, abilities } = pokemonData;
 
   return (
     <div>
       <h2>About Pokemon {name}</h2>
-      <img src={sprites.front_default} alt={`${name} sprite`} />
+      {sprites ? (
+        <img src={sprites.front_default} alt={`${name} sprite`} />
+      ) : (
+        <p>No sprite available</p>
+      )}
       <p>Height: {height}</p>
       <p>Weight: {weight}</p>
       <p>Abilities:</p>
-   
-  
+      <ul>
+        {abilities.map((ability) => (
+          <li key={ability.ability.name}>{ability.ability.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
